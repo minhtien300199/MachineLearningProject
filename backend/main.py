df004cf9beacf0b9a1407746148cfa0790efb6d2
Le Minh Tien <minhtien300199@gmail.com>
2020-12-02 14:29:48 +0700

    add UI for predict image

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
 from flask import Flask, request, jsonify, make_response
+from pathlib import Path
+import os
+from werkzeug import redirect, secure_filename
+from flask.helpers import send_file, url_for
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return redirect(url_for('static', filename='index.html'))
+
+
+@app.route("/process",methods=["POST","GET"])
+def handle_image():
+    if request.method=='GET':
+        return redirect(url_for('static', filename='predict.html'))
+
+    uploaded_file = request.files['file']
+    location = 'upload'
+    if (location.endswith('/') == False):
+        location += "/"
+    if (os.path.exists(location) == False):
+        Path(location).mkdir(parents=True, exist_ok=True)
+    path_name = location + uploaded_file.filename
+    uploaded_file.save(path_name)
+    #do some stuff here
+    return send_file(path_name,mimetype="image/gid");
 
 if __name__ == '__main__':
     app.run()
---
+++ backend/static/scripts/handlePredict.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded',(event)=>{
+    document.querySelector('#exampleFormControlFile1').addEventListener('onChange',(event)=>{
+        console.log(event.target.value);
+
+    })
+});
+
+const renderImage = (e) =>{
+    console.log("load Image!");
+    var imgTag = document.getElementById("user-img");
+    imgTag.src = URL.createObjectURL(e.target.files[0]);
+};
+
