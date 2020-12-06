9a89fbd826498d74f56cb8f8625c667cc27c1174
minhtien300199 <minhtien300199@gmail.com>
2020-12-06 09:54:43 +0700

    add event

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify, make_response
+import  flask
+from flask import Flask, request, jsonify, make_response, redirect
 from pathlib import Path
 import os
-from werkzeug import redirect, secure_filename
+# from werkzeug import redirect
 from flask.helpers import send_file, url_for
 app = Flask(__name__)
 @app.route("/")
@@ -23,7 +24,7 @@ def handle_image():
     path_name = location + uploaded_file.filename
     uploaded_file.save(path_name)
     #do some stuff here
-    return send_file(path_name,mimetype="image/gid");
+    return send_file(path_name,mimetype="image/gid")
 
 if __name__ == '__main__':
     app.run()
--- backend/static/scripts/handlePredict.js
+++ backend/static/scripts/handlePredict.js
@@ -9,5 +9,7 @@ const renderImage = (e) =>{
     console.log("load Image!");
     var imgTag = document.getElementById("user-img");
     imgTag.src = URL.createObjectURL(e.target.files[0]);
+    imgTag.width="200px";
+    imgTag.height="200px";
 };
 
